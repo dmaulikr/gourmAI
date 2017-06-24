ebaf238efeac3da900eb5511f1adeadfa9d7cfde
Zack Esm <zackesm@Zacks-MacBook-Pro.local>
2017-06-24 11:44:16 -0400

    repo init.

---
+++ Gourmai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gourmai
+//
+//  Created by Zack Esm on 6/24/17.
+//  Copyright © 2017 Zack Esm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Gourmai/Pods-Gourmai-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Gourmai : NSObject
+@end
+@implementation PodsDummy_Pods_Gourmai
+@end
